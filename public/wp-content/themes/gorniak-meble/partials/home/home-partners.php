6d3fc4197609ddce279db50f61b9a42cfba6409a
Artur Żaczkowski <artur.zaczkowski@movecloser.pl>
2018-09-03 10:30:18 +0200

    latest project header

--- public/wp-content/themes/gorniak-meble/partials/home/home-partners.php
+++ public/wp-content/themes/gorniak-meble/partials/home/home-partners.php
@@ -1,5 +1,21 @@
+<?php
+$partners_title = get_field('partners_title');
+$partners_subtitle = get_field('partners_subtitle');
+?>
+
+
 <div class="container-fluid partners-container">
     <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="partners-header">
+                    <?= $partners_title ?>
+                </h2>
+                <h3 class="partners-subheader">
+                    <?= $partners_subtitle ?>
+                </h3>
+            </div>
+        </div>
         <div class="row partners-row">
             <?php if (have_rows('repeater_partners')):
                 while (have_rows('repeater_partners')) : the_row();
